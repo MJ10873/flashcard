ded1576cccf661399aeeeb4a22f5db36ef999851
MJ10873 <00001087310064sp@al.educacao.sp.gov.br>
2024-11-05 09:52:10 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Filosofia',
+    'Quais foram as principais ideias de Sócrates??',
+    ' acreditava que a melhor forma de transmitir conhecimento era através da troca direta de ideias através de perguntas e respostas entre duas pessoas.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Noruega?',
+    'A capital da Noruega é Oslo'
+)
+
+criaCartao(
+    'Programação',
+    'O que é JavaScript?',
+    'é uma linguagem de programação que permite a você criar conteúdo que se atualiza dinamicamente'
+)
+
+criaCartao(
+    'Lingua Espanhola',
+    'Como se diz oi tudo bem em Espanhol?',
+    'Oi tudo bem em Espanhol é oye como te va'
+)
